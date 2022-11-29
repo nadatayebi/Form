ffa65fd2e7d88b9f1c2bff8a0c9787733d81b7e9
nadatayebi <nada.tayebi@usmba.ac.ma>
2022-11-29 21:21:53 +0100

    Add project files.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.idBox = new System.Windows.Forms.TextBox();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnConf = new System.Windows.Forms.Button();
+            this.priceBox = new System.Windows.Forms.TextBox();
+            this.autorBox = new System.Windows.Forms.TextBox();
+            this.idlab = new System.Windows.Forms.Label();
+            this.price = new System.Windows.Forms.Label();
+            this.autor = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(4, 34);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(624, 231);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(58, 511);
+            this.btnInsert.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(112, 35);
+            this.btnInsert.TabIndex = 1;
+            this.btnInsert.Text = "insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // idBox
+            // 
+            this.idBox.Location = new System.Drawing.Point(74, 275);
+            this.idBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.idBox.Name = "idBox";
+            this.idBox.Size = new System.Drawing.Size(148, 26);
+            this.idBox.TabIndex = 2;
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(74, 311);
+            this.nameBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(148, 26);
+            this.nameBox.TabIndex = 3;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(180, 511);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(112, 35);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(302, 511);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(112, 35);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(684, 55);
+            this.btnSelect.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(112, 35);
+            this.btnSelect.TabIndex = 7;
+            this.btnSelect.Text = "select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(43, 62);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(179, 28);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(579, 511);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(112, 35);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnConf
+            // 
+            this.btnConf.Location = new System.Drawing.Point(440, 511);
+            this.btnConf.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnConf.Name = "btnConf";
+            this.btnConf.Size = new System.Drawing.Size(112, 35);
+            this.btnConf.TabIndex = 10;
+            this.btnConf.Text = "Confirm";
+            this.btnConf.UseVisualStyleBackColor = true;
+            this.btnConf.Click += new System.EventHandler(this.btnConf_Click);
+            // 
+            // priceBox
+            // 
+            this.priceBox.Location = new System.Drawing.Point(74, 393);
+            this.priceBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.priceBox.Name = "priceBox";
+            this.priceBox.Size = new System.Drawing.Size(148, 26);
+            this.priceBox.TabIndex = 11;
+            this.priceBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // autorBox
+            // 
+            this.autorBox.Location = new System.Drawing.Point(74, 357);
+            this.autorBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.autorBox.Name = "autorBox";
+            this.autorBox.Size = new System.Drawing.Size(148, 26);
+            this.autorBox.TabIndex = 12;
+            // 
+            // idlab
+            // 
+            this.idlab.AutoSize = true;
+            this.idlab.Location = new System.Drawing.Point(21, 281);
+            this.idlab.Name = "idlab";
+            this.idlab.Size = new System.Drawing.Size(26, 20);
+            this.idlab.TabIndex = 13;
+            this.idlab.Text = "ID";
+            // 
+            // price
+            // 
+            this.price.AutoSize = true;
+            this.price.Location = new System.Drawing.Point(16, 399);
+            this.price.Name = "price";
+            this.price.Size = new System.Drawing.Size(26, 20);
+            this.price.TabIndex = 14;
+            this.price.Text = "ID";
+            // 
+            // autor
+            // 
+            this.autor.AutoSize = true;
+            this.autor.Location = new System.Drawing.Point(16, 363);
+            this.autor.Name = "autor";
+            this.autor.Size = new System.Drawing.Size(46, 20);
+            this.autor.TabIndex = 15;
+            this.autor.Text = "autor";
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Location = new System.Drawing.Point(16, 317);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(49, 20);
+            this.name.TabIndex = 16;
+            this.name.Text = "name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1023, 585);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.autor);
+            this.Controls.Add(this.price);
+            this.Controls.Add(this.idlab);
+            this.Controls.Add(this.autorBox);
+            this.Controls.Add(this.priceBox);
+            this.Controls.Add(this.btnConf);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.idBox);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.TextBox idBox;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnConf;
+        private System.Windows.Forms.TextBox priceBox;
+        private System.Windows.Forms.TextBox autorBox;
+        private System.Windows.Forms.Label idlab;
+        private System.Windows.Forms.Label price;
+        private System.Windows.Forms.Label autor;
+        private System.Windows.Forms.Label name;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private int value;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        ////Pour Access----  using System.Data.OleDb;
+        //OleDbConnection AccesConnection = new OleDbConnection();
+        //OleDbDataAdapter AccesAdapter = new OleDbDataAdapter();
+        //String AccesChaineConnection = "Provider=Microsoft.Jet.OLEDB.4.0; Data Source=|DataDirectory|\\BDAcces.mdb;Persist Security Info=True";
+
+        ////Pour SQL Server----using System.Data.SqlClient;
+        //SqlConnection SQLserverConnection = new SqlConnection();
+        //SqlDataAdapter SQLserverAdapter = new SqlDataAdapter();
+        //String SQLserverChaineConnection = "Data Source=.\\SQLEXPRESS;Initial Catalog=BdSQLserver;Integrated Security=True";
+
+        ////Pour MySQL-----using MySql.Data.MySqlClient;
+        //MySqlConnection MySQLConnection = new MySqlConnection();
+        //MySqlDataAdapter MySQLAdapter = new MySqlDataAdapter();
+        //String MySQLChaineConnection = "server=localhost; user id=root;password= ;database=BdMySQL";
+
+        ////Pour Oracle---------using System.Data.OracleClient;
+        //OracleConnection OracleConnection = new OracleConnection();
+        //OracleDataAdapter OracleAdapter = new OracleDataAdapter();
+        //String OracleChaineConnection = "Data Source=BdOracle;User Id=hr;Password=hr";//;DBA Privilege=SYSDBA
+
+
+
+        static string chaine = @"Data Source=DESKTOP-RVGEFLO;Initial Catalog=Demo;Integrated Security=True";
+        static SqlConnection cnx = new SqlConnection(chaine);
+        static SqlCommand cmd = new SqlCommand();
+        static SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnSelect.Enabled = true;
+            btnConf.Enabled = false;
+            btnCancel.Enabled = false;
+            btnDelete.Enabled = false;
+            btnInsert.Enabled = false;
+            btnUpdate.Enabled = false;
+            idBox.Enabled = false;
+            nameBox.Enabled = false;
+            autorBox.Enabled = false;
+            priceBox.Enabled = false;
+            comboBox1.Enabled = true;
+        }
+        int clik = 0;
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            clik++;
+            value = 1;
+            cnx.Open();
+            cmd.Connection = cnx;
+
+            cnx.Close();
+            btnInsert.Enabled = false;
+            btnUpdate.Enabled = false;
+            btnDelete.Enabled = false;
+            btnConf.Enabled = true;
+            btnCancel.Enabled = true;
+            btnSelect.Enabled = false;
+            idBox.Enabled = true;
+            nameBox.Enabled = true;
+            autorBox.Enabled = true;
+            priceBox.Enabled = true;
+            nameBox.Clear();
+            idBox.Clear();
+            autorBox.Clear();
+            priceBox.Clear();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            value = 2;
+            if (cnx.State == ConnectionState.Open)
+            {
+                cnx.Close();
+            }
+            cnx.Open();
+            cmd.Connection = cnx;
+
+            cnx.Close();
+            btnInsert.Enabled = false;
+            btnUpdate.Enabled = false;
+            btnDelete.Enabled = false;
+            btnConf.Enabled = true;
+            btnCancel.Enabled = true;
+            btnSelect.Enabled = false;
+            idBox.Enabled = true;
+            nameBox.Enabled = true;
+            autorBox.Enabled = true;
+            priceBox.Enabled = true;
+            nameBox.Clear();
+            idBox.Clear();
+            autorBox.Clear();
+            priceBox.Clear();
+        }
+        
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            value = 3;
+            cnx.Open();
+            cmd.Connection = cnx;
+
+            cnx.Close();
+            btnInsert.Enabled = false;
+            btnUpdate.Enabled = false;
+            btnDelete.Enabled = false;
+            btnConf.Enabled = true;
+            btnCancel.Enabled = true;
+            btnSelect.Enabled = false;
+            idBox.Enabled = true;
+            nameBox.Enabled = true;
+            autorBox.Enabled = true;
+            priceBox.Enabled = true;
+            nameBox.Clear();
+            idBox.Clear();
+            autorBox.Clear();
+            priceBox.Clear();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+
+            if (cnx.State == ConnectionState.Open)
+            {
+                cnx.Close();
+            }
+            cnx.Open();
+            cmd.CommandText = "select * from Books";
+            cmd.Connection = cnx;
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            comboBox1.DataSource = dt;
+            btnSelect.Enabled = false;
+            btnInsert.Enabled = true;
+            btnUpdate.Enabled = true;
+            btnConf.Enabled = false;
+            btnCancel.Enabled = false;
+            btnDelete.Enabled = false;
+            idBox.Enabled = true;
+            nameBox.Enabled = true;
+            autorBox.Enabled = true;
+            priceBox.Enabled = true;
+            comboBox1.Enabled = true;
+            comboBox1.DisplayMember = "name";
+            comboBox1.ValueMember = "ID";
+            idBox.DataBindings.Clear();
+            nameBox.DataBindings.Clear();
+            autorBox.DataBindings.Clear();
+            priceBox.DataBindings.Clear();
+            idBox.DataBindings.Add("text", comboBox1.DataSource, "ID");
+            nameBox.DataBindings.Add("text", comboBox1.DataSource, "name"); ;
+            autorBox.DataBindings.Add("text", comboBox1.DataSource, "autor");
+            priceBox.DataBindings.Add("text", comboBox1.DataSource, "price");
+            cnx.Close(); 
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConf_Click(object sender, EventArgs e)
+        {
+
+           if (value == 1)
+            {
+                cnx.Open();
+                cmd.Connection = cnx;
+                if (idBox.Text == "" || nameBox.Text == ""  || autorBox.Text == "" ||priceBox.Text == "")
+                { MessageBox.Show("try to fill all the outputs"); }
+                
+          
+                    else
+                    {
+                        cmd.CommandText = "INSERT INTO Books VALUES(" + this.idBox.Text.Trim() + ", '" + this.nameBox.Text.Trim().ToUpper()
+                        + "','" + this.autorBox.Text.Trim().ToLower() + "'," + this.priceBox.Text.Trim() + ")";
+                    int b= cmd.ExecuteNonQuery();
+                    MessageBox.Show(b + " lignes has affected");
+                    Enable();
+                }
+                }
+                else if (value == 2)
+                {
+                    if (idBox.Text == "" || nameBox.Text == "" || autorBox.Text == "" || priceBox.Text == "")
+                    { MessageBox.Show("try t fill all the output"); }
+                    else
+                    {
+                        cnx.Open();
+                        cmd.Connection = cnx;
+                    cmd.CommandText = "Update Books set name='" + nameBox.Text + "',and set autor='" + autorBox.Text + "',and price='" + priceBox.Text + "'" +
+                   " where ID =" + idBox.Text  ;  
+                    int b=cmd.ExecuteNonQuery();
+                    MessageBox.Show(b + " has been affected");
+                    Enable();
+                    cnx.Close();
+                    }
+                }
+                else if (value == 3)
+            {
+                if (idBox.Text == "" || nameBox.Text == "" || autorBox.Text == "" || priceBox.Text == "")
+                { MessageBox.Show("try t fill all the output"); }
+                else
+                    {
+                        cnx.Open();
+                        cmd.Connection = cnx;
+                        cmd.CommandText = "delete from Books where ID='" + idBox.Text + "' ";
+                        int b= cmd.ExecuteNonQuery();
+                    MessageBox.Show(b + " has been deleted");
+                    Enable();
+                        cnx.Close();
+                    }
+                }
+           void Enable()
+            {
+                btnSelect.Enabled = true;
+                btnInsert.Enabled = true;
+                btnUpdate.Enabled = true;
+                btnDelete.Enabled = true;
+                btnConf.Enabled = false;
+                btnCancel.Enabled = false;
+                idBox.Enabled = false;
+                nameBox.Enabled = false;
+                autorBox.Enabled = false;
+                priceBox.Enabled = false;
+                comboBox1.Enabled = false;
+            }
+
+
+            }
+            }
+    }
+
